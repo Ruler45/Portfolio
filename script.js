84617bd30b0844a1d215f6e9a73324ceea918b03
Ruler45 <136236694+Ruler45@users.noreply.github.com>
2023-06-21 21:09:20 +0530

    all files added

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", () => {
+    document.querySelector('.top').classList.toggle('topgo');
+    if (document.querySelector('.top').classList.contains('topgo')) {
+        document.querySelector('.ham').style.display = 'inline';
+        document.querySelector('.cross').style.display = 'none';
+    }
+    else {
+        document.querySelector('.ham').style.display = 'none';
+        setTimeout(() => {
+            document.querySelector('.cross').style.display = 'inline';
+        }, 350);
+    }
+
+
+})
+
+const cnt=document.querySelectorAll(".container");
+
+document.querySelector('.fa-toggle-off').addEventListener("click", () => {
+    setTimeout(() => {
+        document.querySelector('.fa-toggle-off').classList.toggle('fa-toggle-on');
+    }, 300);
+    for(let i=0 ;i<cnt.length;i++)
+    cnt[i].classList.toggle("darkcnt");
+    document.body.classList.toggle("darkbody");
+    document.querySelector(".project").classList.toggle("darkproj");
+    
+    
+})
